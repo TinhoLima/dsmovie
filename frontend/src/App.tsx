4d3feceba9dc9968443b5c3fb423bfa07e5b3de5
TinhoLima <alisonrj.work@gmail.com>
2022-01-11 15:23:38 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
+import Navbar from "./componentes/Navbar";
+
 function App() {
   return (
-    <h1>Página principal</h1>
+    <Navbar/>
   );
 }
 
---
+++ frontend/src/componentes/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GitHubIcon } from 'assets/img/github.svg';
+import './style.css'
+
+function Navbar() {
+    return (
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/tinholima">
+                        <div className='dsmovie-contact-conteiner'>
+                            <GitHubIcon />
+                            <p className='dsmovie-contact-link'>/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
