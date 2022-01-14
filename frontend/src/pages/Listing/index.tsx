927e8e45a2d31e998569106d6917d250e9d57459
TinhoLima <alisonrj.work@gmail.com>
2022-01-14 13:13:45 -0300

    Att final

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,7 +22,7 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`).then(resposta => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`).then(resposta => {
             const data = resposta.data as MoviePage;
             setPage(data);
         })
